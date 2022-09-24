66fee65d7ec85eb1388381d57d41f446cafb61a2
Gilberto Silva <78664060+XxGiillxX@users.noreply.github.com>
2022-09-24 17:54:51 -0300

    Commit Inicial
    Realização do CRUD com C# e .NET6

---
+++ Crud/Crud/Migrations/20220924194324_Crud.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Crud.Migrations
+{
+    public partial class Crud : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Cpf = table.Column<string>(type: "text", nullable: false),
+                    Telefone = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clientes");
+        }
+    }
+}
---
+++ Crud/Crud/Models/BancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Models
+{
+    public class BancoDeDados: DbContext
+    {
+        public DbSet<Cliente> clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString: "Server=127.0.0.1;Port=5432;User Id=admin;Password=admin;Database=postgres;");
+        }
+            
+    }
+}
